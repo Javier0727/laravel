9a200a3a0285fa09cdf73f3e2a6534bc11ec2bdd
javier <javier.castillo@studentpartner.com>
2019-07-05 22:53:24 -0500

    Primeros cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,11 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mis sitios El Javi
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a target="_blank" href="https://code1910.com.mx">code1910.com.mx</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
